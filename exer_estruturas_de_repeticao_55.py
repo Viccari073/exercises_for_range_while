16c26e64f509ff5ea36ab8c9c8bb3f7e9fbb3bba
Lucas Viccari <60453108+Viccari073@users.noreply.github.com>
2020-07-07 01:33:39 -0300

    Add files via upload

---
+++ exer_estruturas_de_repeticao_55.py
@@ -0,0 +1,36 @@
+"""
+Escrever um programa que leia um inteiro não negativo n e imprima a soma dos n primeiros numeros primos.
+"""
+
+
+def test_primo(n):
+    if n == 1:  # 1 não é número primo
+        return False
+    for v in range(2, (int(n/2)+1)):  # Devomos procurar um dividsor de 2 até a metade do número.
+        if n % v == 0:  # Se existir algum número divisor de 2, até a metade do número, não é prim!
+            return False
+    else:
+        return True
+
+
+# Programa principal:
+soma = 0
+numero = int(input('Digite um número inteiro:'))
+
+while numero <= 1:
+    numero = int(input('Digite um número inteiro:'))
+
+for num in range(1, numero):
+    if test_primo(num):
+        soma += num
+
+print(soma)
+
+
+
+
+
+
+
+
+
